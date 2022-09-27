b1bf79f6b8b2a18335dd158b60ebadce6cab0dd4
Viktor Annyenkov <annyenkov@gmail.com>
2022-09-27 16:03:31 +0300

    finish fist task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  body: document.querySelector('body')
+}
+const btnStart = refs.body.children[1];
+const btnStop = refs.body.children[2];
+let intervalId = null;
+btnStop.disabled = true;
 
+function changeBgColorStart() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function changeBgColorStop() {
+  clearInterval(intervalId);
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', changeBgColorStart);
+btnStop.addEventListener('click', changeBgColorStop);
